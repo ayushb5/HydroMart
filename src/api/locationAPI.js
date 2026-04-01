7fbc48228d6c205f534da4365ad4a6b2d1be7267
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-04-01 21:04:45 +0530

    location API

--- src/api/locationAPI.js
+++ src/api/locationAPI.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "https://api.countrystatecity.in/v1",
   headers: {
-    "X-CSCAPI-KEY":
-      "ece7446239c8d39919e176d2e42a9072ae7d4b5f4c48c9fb30de2be70f811e7d",
+    "X-CSCAPI-KEY": "YOUR API KEY",
   },
 });
 
